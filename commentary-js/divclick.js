25e667859ec04e20b03e01b70d1dc11482058061
C. Titus Brown <titus@idyll.org>
2010-05-23 00:19:47 -0400

    got basic mechanisms working, yay

--- commentary-js/divclick.js
+++ commentary-js/divclick.js
@@ -1,36 +1,61 @@
 $(document).ready(function() {
-	$("input#commentary-save").click(function() {
-		alert("fiz!");
-		return false;
-	    });
 
-	$("input#commentary-cancel").click(function() {
-		alert("fiz!");
-		return false;
-	    });
+	$("div.body").find('div').dblclick(function(event) {
+		var newform = $("form#commentary-form").clone(true);
+		newform.addClass('commentary-comment');
 
-	$("div").dblclick(function(event) {
-		newform = $("form#commentary-form").clone().addClass('commentary-comment');
-		newform.id = 'bar';
+		var section_id = this.id;
 
-		$("div#" + this.id).append(newform);
-		$("div#bar").show();
+		newform.find("input#commentary-save").click(function() {
+			var username = newform.find('input#username').val();
+			var email = newform.find('input#email').val();
+			var comment = newform.find('textarea#comment').val();
+
+			newform.hide();
+
+			$.ajax({ url: '/comments/add',
+				    type: 'POST',
+				    data: {
+				    page: 'commentary_this_page',
+					comment: comment,
+					email: email,
+					username: username,
+					position: section_id } });
+
+			add_comment(5, section_id, comment);
+
+			return false;
+		    });
+
+		newform.find("input#commentary-cancel").click(function() {
+			newform.hide();
+			return false;
+		    });
       
+		div = $("div#" + this.id);
+		div.children(':first').before(newform);
+
 		return false;
 	    });
 
+	function add_comment(n, position, comment) {
+	    var form = $("#commentary-display");
+	    var newform = form.clone();
+
+	    comment_id = 'comment-' + n;
+	    newform.attr("id", comment_id);
+	    newform.addClass('commentary-inline');
+	    newform.addClass('commentary-comment');
+	    newform.text(comment);
+	    $('div#' + position).children(':first').before(newform);
+	    
+	};
+
+	
 	$.getJSON('/comments/get', { page: commentary_this_page },
 		  function(data) {
 		$.each(data, function(i, item) {
-			var form = $("#commentary-display");
-			var newform = form.clone();
-
-			comment_id = 'comment-' + item.num;
-			newform.attr("id", comment_id);
-			newform.addClass('commentary-inline');
-			newform.addClass('commentary-comment');
-			newform.text(item.comment);
-			$('div#' + item.position).children(':first').before(newform);
+			add_comment(item.num, item.position, item.comment);
 		    });
 	    });
 });
