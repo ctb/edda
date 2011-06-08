f85e58db267e03658740d8843fa159f4d3520501
C. Titus Brown <t@athyra.local>
2011-06-07 22:15:00 -0400

    make update now does clean/push

--- doc/update-angus.sh
+++ doc/update-angus.sh
@@ -1,3 +1,3 @@
 cp files/events.cfg _build/html/files
-rsync -av _build/html/ t@teckla.idyll.org:/domains/ged.cse.msu.edu/html/angus
+rsync -av _build/html/ angus@teckla.idyll.org:/domains/ged.cse.msu.edu/html/angus
 
